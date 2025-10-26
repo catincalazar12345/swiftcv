188f7be2e24e10a8a9e90c93e0c347ed2e4405f0
unknown <petcurazvam012@gmail.com>
2025-10-26 15:34:18 +0200

    Adauga la numaratoare

---
+++ back-end/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("bagamias pulan maivuta ta sclavule");
+});
+
+app.listen(5000, () => {
+    console.log("Serverul ruleaza pe http://localhost:5000");
+})
